854fbfb2ee2e1a0801ff3bb7ae24a438c5abea77
Lea Marsela <lea.marsela@gmail.com>
2020-09-13 10:34:52 +0700

    Add file html

--- main.js
+++ main.js
@@ -1,1 +1,98 @@
-console.log('test');
+const electron = require('electron');
+const path = require('path');
+const url = require('url');
+
+const { app, BrowserWindow, Menu, ipcMain } = electron;
+
+// set ENV
+process.env.NODE_ENV = 'production';
+
+let mainWindow;
+
+// listen for app to be ready
+app.on('ready', () => {
+    // create new window
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    // load html into window
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    // quit app when closed
+    mainWindow.on('closed', () => {
+        app.quit();
+    });
+
+    // build menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+
+    // insert menu
+    Menu.setApplicationMenu(mainMenu);
+});
+
+
+let addWindow;
+
+// handle create add window
+const createAddWindow = () => {
+    // create new window
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Add Items',
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    // load html into window
+    addWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'addWindow.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    // garbage collection handle
+    addWindow.on('close', () => {
+        addWindow = null;
+    });
+};
+
+
+
+// create menu template
+const mainMenuTemplate = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Add Item',
+                click() {
+                    createAddWindow();
+                }
+            },
+            {
+                label: 'Clear Items',
+                click() {
+                    mainWindow.webContents.send('item:clear');
+                }
+            },
+            {
+                label: 'Quit',
+                accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+                click() {
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
+
+
